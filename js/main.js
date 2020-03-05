7b585bd40d7c66aac17eeb498d7d7ba88c60f0f1
Kevin <kevinpanacci@hotmail.it>
2020-03-05 12:01:04 +0100

    problemi di float ?

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    var d = new Date();
+    var m = d.getMinutes();
+    var h = d.getHours();
     $('#invia').click(function(){
         var nomeInput = $('#chat-input').val();
         $('#chat-input').val('');
-        var messaggio = $('.template #inviato').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
+        var messaggio = $('.template .inviato').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
         messaggio.children('.text').text(nomeInput); // Modifico il testo messaggio nel messaggio
-        messaggio.children('.orario').text('05:05');
+        messaggio.children('.orario').text(h + '.' + m);
         $('.conversazione').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
     });
 });
