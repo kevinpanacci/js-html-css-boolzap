986d0c77bd55da9bda9c3f63997d6f0d22c61504
Kevin <kevinpanacci@hotmail.it>
2020-03-11 10:48:16 +0100

    Aggiunti cambiamenti a chat, e profilo interlocutore. Switch chat non funzionante ??

--- js/main.js
+++ js/main.js
@@ -27,30 +27,17 @@ $(document).ready(function(){
     });
 
 //Selezione chat
-    $('.card').click(function() {
-            $(this).addClass('active');
-            $(this).siblings().removeClass('active');
-    });
+    // $('.card').click(function() {
+    //         $(this).addClass('active');
+    //         $(this).siblings().removeClass('active');
+    // });
+
 
 //dropdown menu
     $(document).on('click', '.fa-angle-down', function() {
         $('.dropdown-menu-msg').toggleClass('invisible');
     });
 
-    // function inviaMessaggio() {
-    //     var d = new Date();
-    //     var m = d.getMinutes();
-    //     var h = d.getHours();
-    //     $('#invia').click(function(){
-    //         var nomeInput = $('#chat-input').val();
-    //         $('#chat-input').val('');
-    //         var messaggio = $('.template .messaggio-da-copiare').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
-    //         messaggio.find('.text').text(nomeInput); // Modifico il testo messaggio nel messaggio
-    //         messaggio.find('.orario').text(h + '.' + m);
-    //         $('.conversazione').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
-    //         setTimeout(riceviMessaggio, 1000)
-    //     });
-    // }
     function inviaMessaggio() {
         var d = new Date();
         var m = d.getMinutes();
@@ -74,5 +61,24 @@ $(document).ready(function(){
         messaggio.find('.orario').text(h + '.' + m);
         $('.conversazione').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
     }
+
+    // Milestone III -- NON FUNZIONERà PERCHè LA RICERCA è CHIAMATA CARD COME LE ALTRE CARD DEI CONTATTI :(
+//Selezione chat attiva
+    $('.card').click(function(){                    //osservo se il contact è stato cliccato
+        $('.card').removeClass('active');
+        $(this).addClass('active');
+        var utenteData = $(this).data('conversazione');          //facciamo una foto valore data-conversazione dell'elemento cliccato, conversazione si riferisce a data-conversazione
+        var chatCorrispondente = $('.conversazione-container[data-conversazione="' + utenteData + '"]') //cerco il relativo valore in Conversazione
+        $('.conversazione-container').removeClass('visible');    //elimino l'active a tutte le conversazioni in conversazione
+        chatCorrispondente.addClass('visible');    //nella conversazione giusta aggiungo l'active
+
+        // PERSONALIZZAZIONE DELL'INTERFACCIA DI CHAT
+        // CAMBIO NOME
+        var nomeContatto = $(this).find('h2').text(); //prendo il nome del contatto sul quale ho cliccato
+        $('.nome-amico h2').text(nomeContatto); //sostituisco il nome con quello della chat cliccata
+        // CAMBIO IMMAGINE
+        var srcContatto = $(this).find('.img-contatto').children('img').attr('src'); //prendo l'immagine cliccata
+        $('.img-profilo-amico').children('img').attr('src', srcContatto); 
+    });
 
 });
