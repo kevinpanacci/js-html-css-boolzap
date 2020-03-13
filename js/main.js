52890b0b2a97839d4547784460f6ee799e0b14ee
Kevin <kevinpanacci@hotmail.it>
2020-03-13 23:28:44 +0100

    Aggiunto switch conversazioni - da fixare inviaMsg

--- js/main.js
+++ js/main.js
@@ -1,23 +1,83 @@
 $(document).ready(function(){
-
-
-// Utilizzo invio per mandare il messaggio
-    $('.chat-input').keydown(function(event) {
+//MILESTONE 1B  ----- INVIA MESSAGGIO CON HANDLEBARS
+    $('#chat-input').keydown(function(event) {
         if(event.keyCode == 13) {
-            inviaMessaggio();
+            creaMsg(testoMessaggio, direzione, selettoreConversazione);
         }
     });
 
-// cambio microfono
-    $('#chat-input').focus(function() {
-        $('#invia').toggleClass('fa fa-microphone fas fa-paper-plane');
-    });
+    for (var convKey in messaggiArchiviati) {
+        var numeroConversazione = convKey[1];
+        for (var i = 0; i < convKey.length; i++) {
+            var oggettoMessaggio = messaggiArchiviati[convKey][i];
+            var testoMessaggio = oggettoMessaggio.testoMessaggio;
+            var direzione = oggettoMessaggio.direzione;
+            var selettoreConversazione = $('.cta-messaggi[data-onversazione="' + numeroConversazione + '"]');
+            creaMsg(testoMessaggio, direzione, selettoreConversazione);
+        }
+    }
+    var messaggiArchiviati = {
+        c0: [
+            {
+                testoMessaggio: 'Ciao Fabio come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'inviato'
+            },
+            {
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'received'
+            }
+        ],
+        c1: [
+            {
+                testoMessaggio: 'Ciao Federico come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'inviato'
+            },
+            {
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'received'
+            }
+        ],
+        c2: [
+            {
+                testoMessaggio: 'Ciao Federica come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'inviato'
+            },
+            {
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'received'
+            }
+        ],
+        c3: [
+            {
+                testoMessaggio: 'Ciao Antonio come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'inviato'
+            },
+            {
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
+                direzione: 'received'
+            }
+        ],
+    };
 
-// RICERCA UTENTE
-    $('#cerca-utente').keyup(function(event) {
+    var source = $('#messaggio-template').html();
+    var template = Handlebars.compile(source);
+    function creaMsg(testoMsg, sentReceived, selettoreConversazione) {
+        var datiMessaggio = {
+            testoMessaggio: testoMsg,
+            direzione: 'inviato'
+        };
+        var templateMessaggio = template(datiMessaggio);
+        $(selettoreConversazione).append(templateMessaggio);
+    }
+
+
+
+
+// MILESTONE 2B ----- RICERCA UTENTE
+        $('.search-input').keyup(function(event) {
         var carattereFiltro = $(this).val();
-        $('.card').each(function() {
-            var x = $(this).find('.nome-contatto h2').text().toLowerCase();
+        $('.contatto').each(function() {
+            var x = $(this).find('.info-contatto h2').text().toLowerCase();
             if (x.includes(carattereFiltro)) {
                 $(this).show();
             } else {
@@ -25,64 +85,21 @@ $(document).ready(function(){
             }
         });
     });
-
-//Selezione chat
-    // $('.card').click(function() {
-    //         $(this).addClass('active');
-    //         $(this).siblings().removeClass('active');
-    // });
-
-
-
-
-    function inviaMessaggio() {
-        var d = new Date();
-        var m = d.getMinutes();
-        var h = d.getHours();
-        var nomeInput = $('#chat-input').val();
-        $('#chat-input').val('');
-        var messaggio = $('.template .messaggio-da-copiare').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
-        messaggio.find('.text').text(nomeInput); // Modifico il testo messaggio nel messaggio
-        messaggio.find('.orario').text(h + '.' + m);
-        $('.conversazione').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
-        setTimeout(riceviMessaggio, 1000)
-
-    }
-
-    function riceviMessaggio() {
-        var d = new Date();
-        var m = d.getMinutes();
-        var h = d.getHours();
-        var messaggio = $('.template-ricevuto .messaggio-da-copiare').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
-        messaggio.find('.text').text('ok'); // Modifico il testo messaggio nel messaggio
-        messaggio.find('.orario').text(h + '.' + m);
-        $('.conversazione').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
-    }
-
-    // Milestone III -- NON FUNZIONERà PERCHè LA RICERCA è CHIAMATA CARD COME LE ALTRE CARD DEI CONTATTI :(
-//Selezione chat attiva
-    $('.card').click(function(){                    //osservo se il contact è stato cliccato
-        $('.card').removeClass('active');
-        $(this).addClass('active');
-        var utenteData = $(this).data('conversazione');          //facciamo una foto valore data-conversazione dell'elemento cliccato, conversazione si riferisce a data-conversazione
-        var chatCorrispondente = $('.conversazione-container[data-conversazione="' + utenteData + '"]') //cerco il relativo valore in Conversazione
-        $('.conversazione-container').removeClass('visible');    //elimino l'active a tutte le conversazioni in conversazione
-        chatCorrispondente.addClass('visible');    //nella conversazione giusta aggiungo l'active
-
-        // PERSONALIZZAZIONE DELL'INTERFACCIA DI CHAT
-// CAMBIO NOME
-        var nomeContatto = $(this).find('h2').text(); //prendo il nome del contatto sul quale ho cliccato
-        $('.nome-amico h2').text(nomeContatto); //sostituisco il nome con quello della chat cliccata
-// CAMBIO IMMAGINE
-        var srcContatto = $(this).find('.img-contatto').children('img').attr('src'); //prendo l'immagine cliccata
-        $('.img-profilo-amico').children('img').attr('src', srcContatto);
-//DROPDOWN MENU - NON FUNZIONANTE (PER I SOLITI PROBLEMI DI HTML E CSS)
-        $(document).on('click', '.fa-angle-down', function() {
-            $('.dropdown-menu-msg').toggleClass('invisible');
-            // $(this).siblings('.dropdown-menu-msg').toggleClass('visible'); da continuare.....
-        });
-//CANCELLA MESSAGGIO
-
     });
 
-});
+// MILESTONE 3 ---- SELEZIONA Chat
+    $('.contatto').click(function(){
+        $('.contatto').removeClass('active');
+        $(this).addClass('active');
+        var utenteData = $(this).data('conversazione');
+        var chatCorrispondente = $('.cta-messaggi[data-conversazione="' + utenteData + '"]')
+        $('.cta-messaggi').removeClass('active'); //se qualcosa non funziona cambiare active con visible e creare css visible con display block
+        chatCorrispondente.addClass('active');
+    // PERSONALIZZAZIONE DELL'INTERFACCIA DI CHAT
+    // CAMBIO NOME
+        var nomeContatto = $(this).find('h2').text(); //prendo il nome del contatto sul quale ho cliccato
+        $('.dati-interlocutore h2').text(nomeContatto); //sostituisco il nome con quello della chat cliccata
+    // CAMBIO IMMAGINE
+        var srcContatto = $(this).find('.immagine-contatto').children('img').attr('src'); //prendo l'immagine cliccata
+        $('.immagine-interlocutore').children('img').attr('src', srcContatto);
+    });
