70663c918cee60d98bf7726646fe930e5794786b
Kevin <kevinpanacci@hotmail.it>
2020-03-14 20:55:39 +0100

    aggiunto invio messaggio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function() {
 //MILESTONE 1B  ----- INVIA MESSAGGIO CON HANDLEBARS
-    $('#chat-input').keydown(function(event) {
-        if(event.keyCode == 13) {
-            creaMsg(testoMessaggio, direzione, selettoreConversazione);
-        }
-    });
-
     for (var convKey in messaggiArchiviati) {
         var numeroConversazione = convKey[1];
         for (var i = 0; i < convKey.length; i++) {
@@ -19,42 +13,42 @@ $(document).ready(function(){
     var messaggiArchiviati = {
         c0: [
             {
-                testoMessaggio: 'Ciao Fabio come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                testoMessaggio: 'Ciao Fabio come stai?',
                 direzione: 'inviato'
             },
             {
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
-                direzione: 'received'
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
+                direzione: 'ricevuto'
             }
         ],
         c1: [
             {
-                testoMessaggio: 'Ciao Federico come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                testoMessaggio: 'Ciao Federico come stai?',
                 direzione: 'inviato'
             },
             {
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
-                direzione: 'received'
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
+                direzione: 'ricevuto'
             }
         ],
         c2: [
             {
-                testoMessaggio: 'Ciao Federica come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                testoMessaggio: 'Ciao Federica come stai?',
                 direzione: 'inviato'
             },
             {
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
-                direzione: 'received'
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',
+                direzione: 'ricevuto'
             }
         ],
         c3: [
             {
-                testoMessaggio: 'Ciao Antonio come stai?',                  //testoMsg è l'input dove scrive il messaggio
+                testoMessaggio: 'Ciao Antonio come stai?',
                 direzione: 'inviato'
             },
             {
-                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  //testoMsg è l'input dove scrive il messaggio
-                direzione: 'received'
+                testoMessaggio: 'Non c\'è male, ma Luca mi sta facendo impazzire',                  
+                direzione: 'ricevuto'
             }
         ],
     };
@@ -70,25 +64,48 @@ $(document).ready(function(){
         $(selettoreConversazione).append(templateMessaggio);
     }
 
+    // INVIO MESSAGGIO (funzione)
+    $('#chat-input').keypress(function(event) {
+        if(event.keyCode == 13) {
+            invioMessaggio();
+        }
+    });
+
+    function invioMessaggio() {
+        var testoMessaggio = $('#chat-input').val();
+        if(testoMessaggio.trim().length > 0) {
+            $('#chat-input').val('');
+            creaMsg(testoMessaggio, 'inviato', '.cta-messaggi.active');
+            scroll();
+            setTimeout(function() {
+                creaMsg('ok', 'ricevuto', '.cta-messaggi.active');
+                scroll();
+            }, 1000);
+        };
+    };
+
+
 
 
 
 // MILESTONE 2B ----- RICERCA UTENTE
-        $('.search-input').keyup(function(event) {
-        var carattereFiltro = $(this).val();
-        $('.contatto').each(function() {
-            var x = $(this).find('.info-contatto h2').text().toLowerCase();
-            if (x.includes(carattereFiltro)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
+    $('.search-input').keyup(function(event) {
+    var carattereFiltro = $(this).val();
+    $('.contatto').each(function() {
+        var x = $(this).find('.info-contatto h2').text().toLowerCase();
+        if (x.includes(carattereFiltro)) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
     });
     });
 
+
+
+
 // MILESTONE 3 ---- SELEZIONA Chat
-    $('.contatto').click(function(){
+    $('.contatto').click(function() {
         $('.contatto').removeClass('active');
         $(this).addClass('active');
         var utenteData = $(this).data('conversazione');
@@ -97,9 +114,12 @@ $(document).ready(function(){
         chatCorrispondente.addClass('active');
     // PERSONALIZZAZIONE DELL'INTERFACCIA DI CHAT
     // CAMBIO NOME
-        var nomeContatto = $(this).find('h2').text(); //prendo il nome del contatto sul quale ho cliccato
-        $('.dati-interlocutore h2').text(nomeContatto); //sostituisco il nome con quello della chat cliccata
+        var nomeContatto = $(this).find('h2').text();
+        $('.dati-interlocutore h2').text(nomeContatto);
     // CAMBIO IMMAGINE
-        var srcContatto = $(this).find('.immagine-contatto').children('img').attr('src'); //prendo l'immagine cliccata
+        var srcContatto = $(this).find('.immagine-contatto').children('img').attr('src');
         $('.immagine-interlocutore').children('img').attr('src', srcContatto);
     });
+
+
+});
